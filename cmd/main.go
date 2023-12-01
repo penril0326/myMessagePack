1cdcdecbbffc9048c11ad50682251af94fc7bbd9
penril0326 <penril0326@gmail.com>
2023-12-02 00:30:20 +0800

    add main and a test case

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/penril0326/myMessagePack/internal/encoder"
+)
 
 func main() {
-	fmt.Println("Hello")
+	// JSON to MessagePack
+	json := map[string]interface{}{
+		"compact": true,
+		"schema":  0,
+	}
+
+	b, err := encoder.JsonToMsgPack(json)
+	if err != nil {
+		log.Fatalf("Convert JSON to msgpack occured error: %s", err.Error())
+	}
+
+	fmt.Printf("%x \n", b)
 }
